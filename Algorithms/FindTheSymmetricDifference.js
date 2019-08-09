6a8b2893542efe80a6f991fbe58fcf7cf354a20b
pmurph0305 <pmurph0305@gmail.com>
2019-08-09 13:17:52 -0400

    Inventory Update

--- Algorithms/FindTheSymmetricDifference.js
+++ Algorithms/FindTheSymmetricDifference.js
@@ -1,3 +1,5 @@
+// https://learn.freecodecamp.org/coding-interview-prep/algorithms/find-the-symmetric-difference/
+
 // Create a function that takes two or more arrays and returns an array
 // of the symmetric difference (△ or ⊕) of the provided arrays.
 
